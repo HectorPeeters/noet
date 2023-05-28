d5df208f5f84f902af3034d719ad4015cc3f3c89
Hector Peeters <hector.peeters@gmail.com>
2023-05-28 14:42:09 +0200

    Add ToFunction for functions with four or five arguments

--- src/function.rs
+++ src/function.rs
@@ -78,3 +78,55 @@ where
         })
     }
 }
+
+impl<'context, A, B, C, D, Context, Value, Func, Return>
+    ToFunction<'context, Context, Value, (A, B, C, D), Return> for Func
+where
+    A: for<'a> Argument<'a, Context, Value>,
+    B: for<'a> Argument<'a, Context, Value>,
+    C: for<'a> Argument<'a, Context, Value>,
+    D: for<'a> Argument<'a, Context, Value>,
+    Func: Fn(&mut Context, Attrs, A, B, C, D) -> Return + 'static,
+    Return: ReturnValue<Value>,
+{
+    fn to_function(self) -> Function<Context, Value> {
+        Box::new(move |evaluator, context, attrs, args| {
+            let mut args = args.into_iter();
+
+            let arg1 = A::from_elements(evaluator, context, &mut args)?;
+            let arg2 = B::from_elements(evaluator, context, &mut args)?;
+            let arg3 = C::from_elements(evaluator, context, &mut args)?;
+            let arg4 = D::from_elements(evaluator, context, &mut args)?;
+
+            let result = self(context, attrs, arg1, arg2, arg3, arg4);
+            Return::to_result_of_option(result)
+        })
+    }
+}
+
+impl<'context, A, B, C, D, E, Context, Value, Func, Return>
+    ToFunction<'context, Context, Value, (A, B, C, D, E), Return> for Func
+where
+    A: for<'a> Argument<'a, Context, Value>,
+    B: for<'a> Argument<'a, Context, Value>,
+    C: for<'a> Argument<'a, Context, Value>,
+    D: for<'a> Argument<'a, Context, Value>,
+    E: for<'a> Argument<'a, Context, Value>,
+    Func: Fn(&mut Context, Attrs, A, B, C, D, E) -> Return + 'static,
+    Return: ReturnValue<Value>,
+{
+    fn to_function(self) -> Function<Context, Value> {
+        Box::new(move |evaluator, context, attrs, args| {
+            let mut args = args.into_iter();
+
+            let arg1 = A::from_elements(evaluator, context, &mut args)?;
+            let arg2 = B::from_elements(evaluator, context, &mut args)?;
+            let arg3 = C::from_elements(evaluator, context, &mut args)?;
+            let arg4 = D::from_elements(evaluator, context, &mut args)?;
+            let arg5 = E::from_elements(evaluator, context, &mut args)?;
+
+            let result = self(context, attrs, arg1, arg2, arg3, arg4, arg5);
+            Return::to_result_of_option(result)
+        })
+    }
+}
